17071c1876862f14e7f46f53d8e2fa6fd211d2d9
himumi <erliansyah@gmail.com>
2025-03-31 21:58:17 +0900

    refactor: create <Part /> component and add it to <Content /> component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,15 +1,10 @@
 const Header = (props) => 
   <h1>{props.course}</h1>;
 
-const Content = (props) => 
+const Part = (props) => 
   <p>{props.part} {props.exercises}</p>;
 
-const Total = (props) => 
-  <p>{props.total}</p>;
-
-const App = () => {
-  const course = 'Half Stack application development';
-  
+const Content = () => {
   const part1 = 'Fundementals of React';
   const exercises1 = 10;
 
@@ -21,10 +16,28 @@ const App = () => {
 
   return (
     <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+    </div>
+  );
+};
+
+const Total = (props) => 
+  <p>{props.total}</p>;
+
+
+const App = () => {
+  const course = 'Half Stack application development';
+  
+  const exercises1 = 10;
+  const exercises2 = 7;
+  const exercises3 = 14;
+
+  return (
+    <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
