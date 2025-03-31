34e4aa119d981b2518c503b180938a68437456a6
himumi <erliansyah@gmail.com>
2025-03-31 21:48:09 +0900

    create <Total /> component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -32,6 +32,9 @@ const Header = (props) =>
 const Content = (props) => 
   <p>{props.part} {props.exercises}</p>;
 
+const Total = (props) => 
+  <p>{props.total}</p>;
+
 const App = () => {
   const course = 'Half Stack application development';
   
@@ -50,6 +53,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
