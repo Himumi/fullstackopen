a8923d6648b475981467dabf11ef8e372807cfab
himumi <erliansyaha@gmail.com>
2025-06-10 17:24:54 +0700

    The phonebook step2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react';
+
 const App = () => {
-  return;
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson))
+
+    setNewName('');
+  };
+
+  const handleNewName = (event) => setNewName(event.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+                  value={newName} 
+                  onChange={handleNewName} 
+                />
+        </div>
+        <div>
+          <button type='submit'>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <li key={person.name}>{person.name}</li>
+      )}
+    </div>
+  );
 };
 
 export default App;
