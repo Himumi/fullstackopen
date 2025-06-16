7b82dd61c91a0c6c12b920e0e14bb48208575ff7
himumi <erliansyaha@gmail.com>
2025-06-16 09:58:26 +0700

    Phonebook backend step 1

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const app = express();
 
+// Persons data
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+];
+
+// Example route
 app.get('/', (request, response) => response.send('Hello World'));
+
+// Route Handlers
+const getPersonsHandler = (request, response) => {
+  response.json(persons);
+};
+
+// Routes
+app.get('/api/persons', getPersonsHandler);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server listening to ${PORT}`));
