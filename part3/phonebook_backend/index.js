987cde3406ab75b5f3505d010729454f569adf7c
himumi <erliansyaha@gmail.com>
2025-06-16 09:47:23 +0700

    install express and create index.js

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => response.send('Hello World'));
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server listening to ${PORT}`));
