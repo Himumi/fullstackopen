9daa34f43fe327318a871ce93eb1bfa78c12810a
himumi <erliansyaha@gmail.com>
2025-06-20 09:39:03 +0700

    Phonebook database step 6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -6,10 +6,6 @@ const app = express();
 
 const Person = require('./models/person');
 
-
-// Example route
-// app.get('/', (request, response) => response.send('Hello World'));
-
 // Route Handlers
 const getPersonsHandler = (request, response, next) => {
   Person
@@ -18,16 +14,11 @@ const getPersonsHandler = (request, response, next) => {
     .catch(error => next(error));
 };
 
-const getPersonHandler = (request, response) => {
-  const id = request.params.id;
-  const person = persons.find(n => n.id === id);
-
-  if (!person) {
-    const errorMessage = { error: 'person is not found' };
-    return response.status(404).json(errorMessage);
-  }
-
-  return response.json(person);
+const getPersonHandler = (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then(result => response.json(result))
+    .catch(error => next(error));
 };
 
 const deletePersonHandler = (request, response, next) => {
@@ -51,11 +42,6 @@ const createPersonHandler = (request, response) => {
     return response.status(404).json(error);
   }
 
-  // if (hasSameName(persons, name)) {
-  //   const error = 'name must be unique';
-  //   return response.status(404).json(error);
-  // }
-
   const newPerson = new Person({ name, number });
 
   newPerson
@@ -78,16 +64,18 @@ const updatePersonHandler = (request, response, next) => {
     .catch(error => next(error));
 };
 
-const getInfoHandler = (request, response) => {
-  const time = new Date;
-  const personsLength = persons.length;
+const getInfoHandler = (request, response, next) => {
+  Person
+    .find({})
+    .then(persons => {
+      const text = `
+        <p>Phonebook has info for ${persons.length} people</p> 
+        <p>${Date().toString()}</p>
+      `;
 
-  const text = `
-    <p>Phonebook has info for ${personsLength} people</p> 
-    <p>${time.toString()}</p>
-  `;
-
-  response.send(text);
+      response.send(text);
+    })
+    .catch(error => next(error));
 };
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
@@ -100,12 +88,15 @@ app.use(express.json());
 app.use(morgan(morganTokens));
 
 // Routes
+app.get('/info', getInfoHandler);
 app.get('/api/persons', getPersonsHandler);
+app.get('/api/persons/:id', getPersonHandler);
+
 app.post('/api/persons', createPersonHandler);
+
 app.delete('/api/persons/:id', deletePersonHandler);
+
 app.put('/api/persons/:id', updatePersonHandler);
-// app.get('/api/persons/:id', getPersonHandler);
-// app.get('/info', getInfoHandler);
 
 // Middleware's Handlers
 const errorHandler = (error,request, response, next) => {
