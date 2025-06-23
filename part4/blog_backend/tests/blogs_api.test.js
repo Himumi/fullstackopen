6a029399aad4cb9fcb5dab7f0977e9c5ba006de3
himumi <erliansyaha@gmail.com>
2025-06-23 17:23:47 +0700

    refactor: reorganize blogs_api tests tests/blogs_api.test.js

--- part4/blog_backend/tests/blogs_api.test.js
+++ part4/blog_backend/tests/blogs_api.test.js
@@ -8,97 +8,79 @@ const helper = require('./blogs_helper');
 
 const api = supertest(app);
 
-describe('blog router', async () => {
+describe('blogs api', () => {
   beforeEach(async () => await helper.resetDB());
 
-  test('api returns all notes', async () => {
-    const result = await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+  describe('getNoteshandler', () => {
+    test('api returns all notes', async () => {
+      const result = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
 
-    assert.strictEqual(result.body.length, helper.initialBlogs.length);
+      assert.strictEqual(result.body.length, helper.initialBlogs.length);
+    });
+
+    test('returned blogs with id property instead _id', async () => {
+      const result = await api.get('/api/blogs');
+      const blog = result.body[0];
+
+      assert(blog.hasOwnProperty('id'));
+      assert(!blog.hasOwnProperty('_id'));
+    });
   });
 
-  test('returned blogs with id property instead _id', async () => {
-    const result = await api.get('/api/blogs');
-    const blog = result.body[0];
+  describe('createNoteHandler', () => {
+    test('returns status 400 when data is incorrect', async () => {
+      const newBlog = { 
+        title: 'test title',
+        author: 'test author',
+        likes: 0
+      };
 
-    assert(blog.hasOwnProperty('id'));
-    assert(!blog.hasOwnProperty('_id'));
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400);
+    });
+
+    test('does not create blog when data is incorrect', async () => {
+      const newBlog = { 
+        title: 'test title',
+        author: 'test author',
+        likes: 0
+      };
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog);
+
+      const blogsAtEnd = await helper.getBlogs();
+      const titles = blogsAtEnd.map(blog => blog.title);
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+      assert(!titles.includes(newBlog.title));
+    });
+
+    test('creates blog with likes 0 when likes property is missing', async () => {
+      const newBlog = {
+        title: 'test title',
+        author: 'test author',
+        url: 'test url',
+      };
+
+      const result = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+      const blogsAtEnd = await helper.getBlogs();
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+      assert.strictEqual(result.body.likes, 0);
+    });
   });
-
-  test('creates blog when data is correct', async () => {
-    const newBlog = {
-      title: 'test title',
-      author: 'test author',
-      url: 'test url',
-      likes: 0
-    };
-
-    const result = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
-    
-    const blogsAtEnd = await helper.getBlogs();
-    const titles = blogsAtEnd.map(blog =>  blog.title);
-
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-    assert(titles.includes(result.body.title));
-
-  });
-
-  test('creates blog with likes 0 when likes property is missing', async () => {
-    const newBlog = {
-      title: 'test title',
-      author: 'test author',
-      url: 'test url',
-    };
-
-    const result = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
-
-    const blogsAtEnd = await helper.getBlogs();
-
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-    assert.strictEqual(result.body.likes, 0);
-  });
-
-  test('returns status 400 when data is incorrect', async () => {
-    const newBlog = { 
-      title: 'test title',
-      author: 'test author',
-      likes: 0
-    };
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400);
-  });
-
-  test('does not create blog when data is incorrect', async () => {
-    const newBlog = { 
-      title: 'test title',
-      author: 'test author',
-      likes: 0
-    };
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog);
-
-    const blogsAtEnd = await helper.getBlogs();
-    const titles = blogsAtEnd.map(blog => blog.title);
-
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
-    assert(!titles.includes(newBlog.title));
-  });
-
-  after(async () => mongoose.connection.close());
 });
+
+after(async () => await mongoose.connection.close());
