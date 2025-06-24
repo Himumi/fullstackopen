9a8ecee11fc38e0b955c860aef8391e7a4c90304
himumi <erliansyaha@gmail.com>
2025-06-24 15:45:02 +0700

    add errorHandler middleware on app

--- part4/blog_backend/app.js
+++ part4/blog_backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const Blog = require('./models/blog');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
+const middleware = require('./utils/middleware');
 
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
@@ -11,9 +12,13 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(middleware.errorHandler);
 
 // Routes
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+
+// middlewares
+app.use(middleware.errorHandler);
 
 module.exports = app;
