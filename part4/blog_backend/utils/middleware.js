43323203e08da3f751e087b79f01117bbb8b4042
himumi <erliansyaha@gmail.com>
2025-06-24 17:29:52 +0700

    refactor: add handler to return error message when username is taken utils/middleware.js

--- part4/blog_backend/utils/middleware.js
+++ part4/blog_backend/utils/middleware.js
@@ -21,8 +21,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(401).json({ error: error.message });
   } else if (error.name === 'MongoServerError'
     && error.message.includes('E11000 duplicate key error')) {
-    return response.status(400).json({
-      error: 'expected `username` to be unique'
+    return response.status(401).json({
+      error: 'User validation failed: username: username must be unique'
     });
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({
