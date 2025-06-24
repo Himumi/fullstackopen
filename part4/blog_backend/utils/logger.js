8ab7f3511ca0300b9e85c04769a969213ae728f1
himumi <erliansyaha@gmail.com>
2025-06-24 17:25:49 +0700

    refactor: logger to print log except test enviroment utils/logger.js

--- part4/blog_backend/utils/logger.js
+++ part4/blog_backend/utils/logger.js
@@ -1,4 +1,17 @@
-const info = (...params) => console.log(...params);
-const error = (...params) => console.error(...params);
+require('dotenv').config();
+
+const nodeEnv = process.env.NODE_ENV;
+
+const info = (...params) => {
+  if (nodeEnv !== 'test') {
+    console.log(...params);
+  }
+};
+
+const error = (...params) => {
+  if (nodeEnv !== 'test') {
+    console.error(...params)
+  }
+};
 
 module.exports = { info, error };
