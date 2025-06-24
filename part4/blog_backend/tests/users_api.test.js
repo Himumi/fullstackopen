5ab6a8bb8174888572796140728dc6fca07ecaf6
himumi <erliansyaha@gmail.com>
2025-06-24 14:48:26 +0700

    test: succeeds create a new user in tests/users_api.test.js

---
+++ part4/blog_backend/tests/users_api.test.js
@@ -0,0 +1,46 @@
+const { test, after, beforeEach, describe } = require('node:test');
+const assert = require('node:assert');
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const app = require('../app');
+const User = require('../models/user');
+const helper = require('./users_helper');
+
+const api = supertest(app);
+
+describe('createUserHandler', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('rahasia', 10);
+    const user = new User({ username: 'root', name: 'Superuser', passwordHash });
+
+    await user.save();
+  });
+
+  test.only('succeeds when data is correct', async () => {
+    const usersAtBegin = await helper.getUsers();
+
+    const newUser = {
+      username: 'himumi',
+      name: 'Himumi Erliansyah',
+      password: 'sangatrahasia'
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+    
+    const usersAtEnd = await helper.getUsers();
+    assert.strictEqual(usersAtEnd.length, usersAtBegin.length + 1);
+
+    const usernames = usersAtEnd.map(u => u.username);
+    assert(usernames.includes(newUser.username));
+  });
+});
+
+after(async () => await mongoose.connection.close());
