760ff4c3f36a65072898e1a09f10cfcf5325a478
himumi <erliansyaha@gmail.com>
2025-06-25 11:10:00 +0700

    refactor: separate error validator and change else-if to switch case for readability
    utils/middleware.js

--- part4/blog_backend/utils/middleware.js
+++ part4/blog_backend/utils/middleware.js
@@ -12,29 +12,47 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
+// conditional functions
+const castError = error => error.name === 'CastError';
+const validationError = error => error.name === 'ValidationError';
+const duplicatedError = error => error.name === 'MongoServerError' 
+  && error.message.includes('E11000 duplicate key error');
+const jwtError = error => error.name === 'JsonWebTokenError';
+const tokenExpiredError = error => error.name === 'TokenExpiredError';
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  }else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  } else if (error.name === 'MongoServerError'
-    && error.message.includes('E11000 duplicate key error')) {
-    return response.status(400).json({
-      error: 'User validation failed: username: username must be unique'
-    });
-  } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({
-      error: 'token invalid'
-    });
-  } else if (error.name === 'TokenExpiredError') {
-    return response.status(401).json({
-      error: 'token expired'
-    });
+  switch (true) {
+    case castError(error): {
+      return response.status(400).json({
+        error: 'malformatted id'
+      });
+    }
+    case validationError(error): {
+      return response.status(400).json({
+        error: error.message
+      });
+    }
+    case duplicatedError(error): {
+      return response.status(400).json({
+        error: 'User validation failed: username: username must be unique'
+      });
+    }
+    case jwtError(error): {
+      return response.status(401).json({
+        error: 'token invalid'
+      });
+    }
+    case tokenExpiredError(error): {
+      return response.status(401).json({
+        error: 'token expired'
+      });
+    }
+    default: {
+      return next(error);
+    }
   }
-
-  next(error);
 };
 
 module.exports = {
