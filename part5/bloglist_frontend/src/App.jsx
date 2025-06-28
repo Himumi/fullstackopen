7865741eb521f01f73176f498fb37332c056a51f
himumi <erliansyaha@gmail.com>
2025-06-28 15:40:53 +0700

    Blog list frontend step 3

--- part5/bloglist_frontend/src/App.jsx
+++ part5/bloglist_frontend/src/App.jsx
@@ -8,6 +8,9 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -20,6 +23,7 @@ const App = () => {
     if (loggedUser) {
       const user = JSON.parse(loggedUser)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -36,6 +40,7 @@ const App = () => {
         'loggedBlogAppUser',
         JSON.stringify(user)
       )
+      blogService.setToken(user.token)
 
       setUser(user)
       setUsername('')
@@ -49,6 +54,7 @@ const App = () => {
   const logoutHandler = () => {
     setUser(null)
     window.localStorage.removeItem('loggedBlogAppUser')
+    blogService.deleteToken()
   }
 
   const usernameHandler = (event) =>
@@ -94,9 +100,69 @@ const App = () => {
         {user.name} logged in
         <button onClick={logoutHandler}>logout</button>  
       </p>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  const titleHandler = ({ target }) => setTitle(target.value)
+  const authorHandler = ({ target }) => setAuthor(target.value)
+  const urlHandler = ({ target }) => setUrl(target.value)
+
+  const createBlogHandler = async (event) => {
+    event.preventDefault()
+
+    try {
+      const blog = await blogService.create({
+        title, author, url
+      })
+      setBlogs(blogs.concat(blog))
+    } catch (error) {
+      console.log(error)
+    }
+
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  const blogForm = () => (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={createBlogHandler}>
+        <div>
+          title: 
+          <input 
+            type='text'
+            name='Title'
+            value={title}
+            onChange={titleHandler}
+          />
+        </div>
+        <div>
+          author: 
+          <input 
+            type='text'
+            name='Author'
+            value={author}
+            onChange={authorHandler}
+          />
+        </div>
+        <div>
+          url: 
+          <input 
+            type='text'
+            name='Url'
+            value={url}
+            onChange={urlHandler}
+          />
+        </div>
+        <div>
+          <button type='submit'>create</button>
+        </div>
+      </form>
     </div>
   )
 
