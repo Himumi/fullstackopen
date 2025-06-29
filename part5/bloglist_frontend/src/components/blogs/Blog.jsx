d9494a8b67351fc26bc9440c6b07a6dfcaad79ae
himumi <erliansyaha@gmail.com>
2025-06-29 22:22:22 +0700

    Blog list frontend step 7

--- part5/bloglist_frontend/src/components/blogs/Blog.jsx
+++ part5/bloglist_frontend/src/components/blogs/Blog.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleVisibility = () => setVisible(!visible)
+
   return (
-    <div>
-      {blog.title} by {blog.author}
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+        <div style={showWhenVisible}>
+          {blog.url} <br />
+          likes {blog.likes} 
+          <button>like</button> <br />
+          {blog.author}
+        </div>
+      </div>
     </div>
   )
 }
