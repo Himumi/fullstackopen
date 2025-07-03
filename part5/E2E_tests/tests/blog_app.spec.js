05c8f1e5466799241f336f23e4024adcf23dd8e9
himumi <erliansyaha@gmail.com>
2025-07-03 10:49:08 +0700

    test: succeeds logged in to app

--- part5/E2E_tests/tests/blog_app.spec.js
+++ part5/E2E_tests/tests/blog_app.spec.js
@@ -16,4 +16,12 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('log in to application')).toBeVisible()
   })
+
+  test('succeeds logged in', async ({ page }) => {
+    await page.getByTestId('username').fill('himumi')
+    await page.getByTestId('password').fill('rahasia')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Erliansyah logged in')).toBeVisible()
+  })
 })
