7369639bd373dc4d389b32bcc4966bd7c5e64b2f
himumi <erliansyaha@gmail.com>
2025-07-03 14:18:47 +0700

    test: succeeds creating a new blog

--- part5/E2E_tests/tests/blog_app.spec.js
+++ part5/E2E_tests/tests/blog_app.spec.js
@@ -2,7 +2,7 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('/testing/reset')
+    await request.post('/api/testing/reset')
     await request.post('/api/users', {
       data: {
         name: 'Erliansyah',
@@ -39,5 +39,23 @@ describe('Blog app', () => {
         'Wrong username or password'
       )).toBeVisible()
     })
+  })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByTestId('username').fill('himumi')
+      await page.getByTestId('password').fill('rahasia')
+      await page.getByRole('button', { name: 'login' }).click()
     })
+
+    test('succeeds creating a new blog', async ({ page }) => {
+      await page.getByRole('button', { name: 'new blog' }).click()
+      await page.getByTestId('titleInput').fill('title')
+      await page.getByTestId('authorInput').fill('author')
+      await page.getByTestId('urlInput').fill('url')
+      await page.getByRole('button', { name: 'create' }).click()
+
+      await expect(page.locator('.blog>span')).toBeVisible()
+    })
+  })
 })
