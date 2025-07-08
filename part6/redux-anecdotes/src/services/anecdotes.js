dae18582c362efefcd983a2441633688ad4fdba8
himumi <erliansyaha@gmail.com>
2025-07-08 15:41:53 +0700

    refactor: create getById and update api in anecdoteService

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -7,6 +7,11 @@ const getAll = async () => {
   return response.data
 }
 
+const getById = async id => {
+  const response = await axios.get(`${baseUrl}/${id}`)
+  return response.data
+}
+
 const createNew = async (content) => {
   const anecdote = {
     content,
@@ -16,4 +21,9 @@ const createNew = async (content) => {
   return response.data
 }
 
-export default { getAll, createNew }
+const update = async (id, object) => {
+  const response = await axios.put(`${baseUrl}/${id}`, object)
+  return response.data
+}
+
+export default { getAll, getById, createNew, update }
