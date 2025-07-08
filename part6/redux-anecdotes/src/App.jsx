1029a96f7ca0c8330acfc8148ebafad7a5813353
himumi <erliansyaha@gmail.com>
2025-07-08 11:52:56 +0700

    refactor: fetch anecdotes from server and set to redux store
    Anecdotes and the backend step 1

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  const fetchAnecdotes = () => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  } 
+
+  // Fetch all Anecdotes from server and set to redux store
+  useEffect(fetchAnecdotes, [])
+
   return (
     <div>
       <h2>Anecdotes</h2>
