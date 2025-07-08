91a1cca53c72667a815be4c00a86f78eeed9597a
himumi <erliansyaha@gmail.com>
2025-07-08 15:31:35 +0700

    refactor: replace setAnecdotes to initializeAnecdotes
    Anecdotes and the backend step 3

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
@@ -13,9 +12,7 @@ const App = () => {
   const dispatch = useDispatch()
 
   const fetchAnecdotes = () => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   } 
 
   // Fetch all Anecdotes from server and set to redux store
