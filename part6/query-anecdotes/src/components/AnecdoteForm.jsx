f5fcc3db58dd735a52153cedc193e156244bb4f9
himumi <erliansyaha@gmail.com>
2025-07-09 11:30:40 +0700

    refactor: create a new anecdote using react query
    Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,11 +1,30 @@
+import { useQueryClient, useMutation } from "@tanstack/react-query"
+import { createAnecdote } from "../services/anecdotes"
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const appendNewAnecdote = newAnecdote => {
+    const anecdotes = queryClient.getQueryData(['anecdotes'])
+    queryClient.setQueryData(
+      ['anecdotes'],
+      anecdotes.concat(newAnecdote)
+    )
+  }
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: appendNewAnecdote,
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+
+    // Post anecdote to server
+    newAnecdoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote')
-}
+  }
 
   return (
     <div>
