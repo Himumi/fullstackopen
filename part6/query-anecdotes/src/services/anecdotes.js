298fea31ea851d044c796cf3a5fc0f9ca2e88d31
himumi <erliansyaha@gmail.com>
2025-07-09 11:47:59 +0700

    refactor: create getAnecdotes service

--- part6/query-anecdotes/src/services/anecdotes.js
+++ part6/query-anecdotes/src/services/anecdotes.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes/'
 
+export const getAnecdotes = () => axios
+  .get(baseUrl)
+  .then(response => response.data)
+
 export const createAnecdote = newAnecdote =>
   axios
     .post(baseUrl, newAnecdote)
