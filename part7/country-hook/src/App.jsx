46ce47247590128ec655b84b25c3a9112ab3d340
himumi <erliansyaha@gmail.com>
2025-07-11 09:43:47 +0700

    refactor: create useCountry logic
    Country hook

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -18,7 +18,27 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name !== '') {
+      const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/name/' 
+      axios
+        .get(`${baseUrl + name}`)
+        .then(response => {
+          setCountry({
+            found: true,
+            data: {
+              name: response.data.name.common,
+              capital: response.data.capital[0],
+              population: response.data.population,
+              flag: response.data.flags.png
+            }
+          })
+        })
+        .catch(() => {
+          setCountry({ found: false })
+        })
+    }
+  }, [name])
 
   return country
 }
@@ -41,7 +61,7 @@ const Country = ({ country }) => {
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
       <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <img style={{ border: '1px solid black'}} src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
     </div>
   )
 }
