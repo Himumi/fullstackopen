f195d85dfbdb88dbd2e3c27e6e49d15e7a668352
himumi <erliansyaha@gmail.com>
2025-07-12 16:46:36 +0700

    copy bloglist-backend, bloglist-frontend, and E2E-tests from part 5

---
+++ part7/bloglist/bloglist-backend/utils/logger.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const nodeEnv = process.env.NODE_ENV;
+
+const info = (...params) => {
+  if (nodeEnv !== 'test') {
+    console.log(...params);
+  }
+};
+
+const error = (...params) => {
+  if (nodeEnv !== 'test') {
+    console.error(...params)
+  }
+};
+
+module.exports = { info, error };
---
+++ part7/bloglist/bloglist-frontend/src/components/Main.jsx
@@ -0,0 +1,46 @@
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import Blogs from './blogs/Blogs'
+import BlogForm from './blogs/BlogForm'
+import Togglable from './Togglable'
+
+// eslint-disable-next-line react/display-name
+const Main = forwardRef((props, ref) => {
+  const {
+    user,
+    handleLogout,
+    handleCreateBlog,
+    handleRemoveBlog,
+    handleUpdateBlog,
+    blogs
+  } = props
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <div>
+        <span>{user.name} logged in</span>
+        <button onClick={handleLogout}>logout</button>
+      </div>
+      <Togglable textLabel='new blog' ref={ref}>
+        <BlogForm handleCreateBlog={handleCreateBlog} />
+      </Togglable>
+      <Blogs
+        handleUpdateBlog={handleUpdateBlog}
+        handleRemoveBlog={handleRemoveBlog}
+        blogs={blogs}
+      />
+    </div>
+  )
+})
+
+Main.propTypes = {
+  blogs: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
+  handleLogout: PropTypes.func.isRequired,
+  handleCreateBlog: PropTypes.func.isRequired,
+  handleUpdateBlog: PropTypes.func.isRequired,
+  handleRemoveBlog: PropTypes.func.isRequired,
+}
+
+export default Main
