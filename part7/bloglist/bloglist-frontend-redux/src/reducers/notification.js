bf5050ae76df74008a69f0c40fcef08c2d67e928
himumi <erliansyaha@gmail.com>
2025-07-12 19:12:12 +0700

    create notificationReducer with setMessage and removeMessage actions

---
+++ part7/bloglist/bloglist-frontend-redux/src/reducers/notification.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  message: '',
+  status: '',
+}
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    setMessage(state, action) {
+      return action.payload
+    },
+    removeMessage(state, action) {
+      return initialState
+    }
+  }
+})
+
+export const { setMessage, removeMessage } = notificationSlice.actions
+export default notificationSlice.reducer
