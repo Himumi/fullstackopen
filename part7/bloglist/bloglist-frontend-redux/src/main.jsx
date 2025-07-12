a332de04b701a89c9026e7cc39086890a1469128
himumi <erliansyaha@gmail.com>
2025-07-13 06:28:58 +0700

    refactor: move store logic from main.jsx to its separate file

--- part7/bloglist/bloglist-frontend-redux/src/main.jsx
+++ part7/bloglist/bloglist-frontend-redux/src/main.jsx
@@ -1,16 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-import notificationReducer from './reducers/notification'
-import blogsReducer from './reducers/blogs'
-
-const store = configureStore({
-  reducer: {
-    notification: notificationReducer,
-    blogs: blogsReducer
-  }
-})
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Provider store={store}>
---
+++ part7/bloglist/bloglist-frontend-redux/src/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import notificationReducer from './reducers/notification'
+import blogsReducer from './reducers/blogs'
+
+const store = configureStore({
+  reducer: {
+    notification: notificationReducer,
+    blogs: blogsReducer
+  }
+})
+
+export default store
