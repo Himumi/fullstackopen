16910f82a3fe00b3c52bae0fb5dd28eb4377d760
himumi <erliansyaha@gmail.com>
2025-07-12 21:19:08 +0700

    test: succeeds to set blogs to store

--- part7/bloglist/bloglist-frontend-redux/src/reducers/blogs.test.js
+++ part7/bloglist/bloglist-frontend-redux/src/reducers/blogs.test.js
@@ -68,4 +68,27 @@ describe('Blogs reducer', () => {
 
     expect(newState).toHaveLength(0)
   })
+
+  test('succeeds to set blogs with blogs/setBlogs', () => {
+    const state = []
+    const action = {
+      type: 'blogs/setBlogs',
+      payload: [
+        {
+          title: 'title',
+          author: 'author',
+          url: 'url',
+          id: 1,
+          likes: 0,
+        }
+      ]
+    }
+
+    deepFreeze(state)
+
+    const newState = reducer(state, action)
+
+    expect(newState).toHaveLength(1)
+    expect(newState[0]).toStrictEqual(action.payload[0])
+  })
 })
