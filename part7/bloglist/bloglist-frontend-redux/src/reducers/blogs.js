2ce8a8c80dfc5f9b4cdd901ee40c9d6e50e2cb61
himumi <erliansyaha@gmail.com>
2025-07-12 21:09:22 +0700

    refactor: create blogs/remove action

--- part7/bloglist/bloglist-frontend-redux/src/reducers/blogs.js
+++ part7/bloglist/bloglist-frontend-redux/src/reducers/blogs.js
@@ -11,9 +11,13 @@ const blogsSlice = createSlice({
       return state.map(b => 
         b.id !== action.payload.id ? b : action.payload
       )
+    },
+    remove(state, action) {
+      const id = action.payload
+      return state.filter(b => b.id !== id)
     }
   }
 })
 
-const { create, update } = blogsSlice.actions
+const { create, update, remove } = blogsSlice.actions
 export default blogsSlice.reducer
