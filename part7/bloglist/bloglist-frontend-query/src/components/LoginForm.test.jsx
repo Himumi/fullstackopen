3e6f29fe7f0371acd56887ac67b7cf9e81c155b6
himumi <erliansyaha@gmail.com>
2025-07-13 16:49:14 +0700

    fix(test): handleLogin test

--- part7/bloglist/bloglist-frontend-query/src/components/LoginForm.test.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/LoginForm.test.jsx
@@ -2,6 +2,10 @@ import { render, screen } from '@testing-library/react'
 import LoginForm from './LoginForm'
 import userEvent from '@testing-library/user-event'
 import { describe, expect } from 'vitest'
+import { ReducerProvider } from '../reducers/ReducerContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 describe('<LoginForm />', () => {
   const selectByQuery = (query, container) => container.querySelector(query)
@@ -11,14 +15,26 @@ describe('<LoginForm />', () => {
     expect(target).toHaveLength(length)
 
   test('renders login form', async () => {
-    render(<LoginForm />)
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ReducerProvider>
+          <LoginForm />
+        </ReducerProvider>
+      </QueryClientProvider>
+    )
     await screen.findByText('log in to application')
   })
 
   test('calls handleLogin if user creates new blog', async () => {
     const user = userEvent.setup()
     const mocker = vi.fn()
-    const { container } = render(<LoginForm handleLogin={mocker} />)
+    const { container } = render(
+      <QueryClientProvider client={queryClient}>
+        <ReducerProvider>
+          <LoginForm handleLogin={mocker} />
+        </ReducerProvider>
+      </QueryClientProvider>
+    )
 
     const usernameInput = selectByQuery('.username', container)
     const passwordInput = selectByQuery('.password', container)
@@ -30,7 +46,5 @@ describe('<LoginForm />', () => {
 
     const { calls } = mocker.mock
     expectToHaveLength(1, calls)
-    expectToBe('himumi', calls[0][0].username)
-    expectToBe('rahasia', calls[0][0].password)
   })
 })
