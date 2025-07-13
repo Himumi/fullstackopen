5a0be74b787f5c3c516cea9884a86d9065ef2da4
himumi <erliansyaha@gmail.com>
2025-07-13 14:09:56 +0700

    refactor: add userReducer to rootReducer

--- part7/bloglist/bloglist-frontend-query/src/reducers/root.js
+++ part7/bloglist/bloglist-frontend-query/src/reducers/root.js
@@ -1,8 +1,10 @@
 import notificationReducer from "./notification";
+import userReducer from './user'
 
 const rootReducer = (state, action) => {
   return {
-    notification: notificationReducer(state.notification, action)
+    notification: notificationReducer(state.notification, action),
+    user: userReducer(state.user, action)
   }
 }
 
