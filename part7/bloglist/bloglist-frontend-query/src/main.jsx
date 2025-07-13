8dcba12181ff8938ecc3b99c39c27b4a7279bee8
himumi <erliansyaha@gmail.com>
2025-07-13 11:06:32 +0700

    refactor: wrap App with ReducerProvider

--- part7/bloglist/bloglist-frontend-query/src/main.jsx
+++ part7/bloglist/bloglist-frontend-query/src/main.jsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ReducerProvider } from './reducers/ReducerContext'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+<ReducerProvider>
+  <App />
+</ReducerProvider>
+)
