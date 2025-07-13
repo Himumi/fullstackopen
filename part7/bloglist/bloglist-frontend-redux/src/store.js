52e7ab89c386d4fa814bb5a9b80c76a1368fefec
himumi <erliansyaha@gmail.com>
2025-07-13 08:58:34 +0700

    refactor: add userReducer to redux store

--- part7/bloglist/bloglist-frontend-redux/src/store.js
+++ part7/bloglist/bloglist-frontend-redux/src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import notificationReducer from './reducers/notification'
 import blogsReducer from './reducers/blogs'
+import userReducer from './reducers/user'
 
 const reducers = {
   notification: notificationReducer,
-  blogs: blogsReducer
+  blogs: blogsReducer,
+  user: userReducer,
 }
 
 const store = configureStore({
