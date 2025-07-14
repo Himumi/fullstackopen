7e58f3e865324ca9b57fa122219e57cce70d35b5
himumi <erliansyaha@gmail.com>
2025-07-14 14:57:09 +0700

    refactor: use useQueryBlogs in Home to retrieve blogs

--- part7/bloglist/bloglist-frontend-query/src/components/Home.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/Home.jsx
@@ -1,14 +1,22 @@
 import Togglable from "./Togglable"
 import BlogForm from "./blogs/BlogForm"
-import Blogs from "./blogs/Blogs"
+import BlogsList from "./blogs/BlogsList"
+
+import useQueryBlogs from "../hooks/useQueryBlogs"
 
 const Home = () => {
+  const result = useQueryBlogs()
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
   return (
     <div>
       <Togglable textLabel="new blog" >
         <BlogForm />
       </Togglable>
-      <Blogs />
+      <BlogsList />
     </div>
   )
 }
