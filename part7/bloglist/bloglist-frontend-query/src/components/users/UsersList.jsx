8c1f6e01e883568eb6e5d8120fe833615ee149e1
himumi <erliansyaha@gmail.com>
2025-07-14 14:20:12 +0700

    create UsersList component

---
+++ part7/bloglist/bloglist-frontend-query/src/components/users/UsersList.jsx
@@ -0,0 +1,28 @@
+import { useQueryClient } from "@tanstack/react-query"
+
+const UserList = () => {
+  const queryClient = useQueryClient()
+  const users = queryClient.getQueryData(['users'])
+
+  return (
+    <div>
+      <h2>Users</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+            <td><strong>blogs created</strong></td>
+          </tr>
+          {users.map(user => 
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default UserList
