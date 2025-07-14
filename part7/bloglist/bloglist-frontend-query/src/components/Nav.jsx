58c958abc3d0e1a3ac687ae23dc284607305473e
himumi <erliansyaha@gmail.com>
2025-07-14 15:45:13 +0700

    refactor: add Link to users and blogs, and add some styles
    Navigation

--- part7/bloglist/bloglist-frontend-query/src/components/Nav.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/Nav.jsx
@@ -1,13 +1,30 @@
+import { Link } from "react-router-dom"
 import useUser from "../hooks/useUser"
 
 const Nav = () => {
   const user = useUser()
 
+  const navStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: 10,
+    height: 50,
+    paddingLeft: 15,
+    background: 'lightgray',
+  }
+
   return (
-    <div>
-      <h2>blogs</h2>
+    <div style={navStyles}>
+      <div>
+        <Link to='/blogs'>blogs</Link>
+      </div>
+      <div>
+        <Link to='/users'>users</Link>
+      </div>
       <div>
         <span>{user.value.name} logged in</span>
+      </div>
+      <div>
         <button onClick={user.handleLogout}>logout</button>
       </div>
     </div>
