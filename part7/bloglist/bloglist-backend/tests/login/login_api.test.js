b51f1874342cf48b856d53052b80fd9096851546
himumi <erliansyaha@gmail.com>
2025-07-15 06:56:23 +0700

    refactor(test): reorganize login api tests

--- part7/bloglist/bloglist-backend/tests/login/login_api.test.js
+++ part7/bloglist/bloglist-backend/tests/login/login_api.test.js
@@ -5,44 +5,48 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 
 const app = require('../../app')
-const { resetDB, removeAllInfo } = require('../helper/helper')
+const usersHelper = require('../users/users_helper')
 
 const api = supertest(app)
 
-describe('loginHandler', () => {
+describe('login api', () => {
   beforeEach(async () => {
-    await resetDB()
+    await usersHelper.resetDB()
   })
 
-  test('succeeds login when all data are correct', async () => {
-    const login = {
-      username: 'root',
-      password: 'rahasia'
-    }
+  describe('succeeds login', () => {
+    test('when all data are correct', async () => {
+      const login = {
+        username: 'root',
+        password: 'rahasia'
+      }
 
-    const result = await api
-      .post('/api/login')
-      .send(login)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/login')
+        .send(login)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(result.body.username, login.username)
+      assert.strictEqual(result.body.username, login.username)
+    })
   })
 
-  test('fails login when one of data missing (bad request)', async () => {
-    const login = { username: 'root' }
+  describe('fails login', () => {
+    test('when one of data missing (bad request)', async () => {
+      const login = { username: 'root' }
 
-    const result = await api
-      .post('/api/login')
-      .send(login)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
+      const result = await api
+        .post('/api/login')
+        .send(login)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
 
-    assert(result.body.error.includes('missing username or password'))
+      assert(result.body.error.includes('missing username or password'))
+    })
   })
 
   after(async () => {
-    await removeAllInfo()
+    await usersHelper.removeAllInfo()
     await mongoose.connection.close()
   })
 })
