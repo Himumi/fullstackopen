41147d526d51ea5b9d32fb9116cffb604bf5d8a5
himumi <erliansyaha@gmail.com>
2025-07-14 14:27:57 +0700

    refactor: add conditional rendering to User when user is not found will render nothing
    Individual user view

--- part7/bloglist/bloglist-frontend-query/src/components/users/User.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/users/User.jsx
@@ -8,6 +8,10 @@ const User = () => {
   const users = queryClient.getQueryData(['users'])
   const user = users.find(user => user.id === id)
 
+  if (!user) {
+    return null
+  }
+
   return (
     <div>
       <h2>{user.name}</h2>
