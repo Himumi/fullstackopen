46e1c672f56eba7a575040a6c987ca9ef8529f14
himumi <erliansyaha@gmail.com>
2025-07-17 20:24:23 +0700

    ui: add styles to UserList

--- part7/bloglist/bloglist-frontend-query/src/components/users/UsersList.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/users/UsersList.jsx
@@ -1,3 +1,4 @@
+import { Typography } from "@material-tailwind/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link } from "react-router-dom"
 
@@ -6,20 +7,29 @@ const UserList = () => {
   const users = queryClient.getQueryData(['users'])
 
   return (
-    <div>
-      <h2>Users</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td></td>
-            <td><strong>blogs created</strong></td>
+    <div className="w-3/4 mx-auto my-36">
+      <table className="w-full">
+        <thead className="border border-surface bg-surface-light text-lg font-medium text-foreground ">
+          <tr className="h-16">
+            <th className="border-r">users</th>
+            <th>blogs created</th>
           </tr>
+        </thead>
+        <tbody>
           {users.map(user => 
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`} >{user.name}</Link>
+            <tr key={user.id} className="border-b border-surface h-16">
+              <td className="p3 text-center border-l">
+                <Link to={`/users/${user.id}`} >
+                  <Typography className='font-normal'>
+                    {user.name}
+                  </Typography>
+                </Link>
               </td>
-              <td>{user.blogs.length}</td>
+              <td className="p3 text-center border-l border-r">
+                <Typography className='font-normal'>
+                  {user.blogs.length}
+                </Typography>
+              </td>
             </tr>
           )}
         </tbody>
