8e63f13d9ab188adecde3f449f21fbb38fe0bc58
himumi <erliansyaha@gmail.com>
2025-07-17 18:59:08 +0700

    ui: add styles to BlogsList

--- part7/bloglist/bloglist-frontend-query/src/components/blogs/BlogsList.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/blogs/BlogsList.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useBlogsQuery } from '../../hooks/useBlogsQuery'
+import { Typography } from '@material-tailwind/react'
 
 const Blogs = () => {
   const result = useBlogsQuery()
@@ -14,21 +15,14 @@ const Blogs = () => {
 
   const blogs = result.data
 
-  const blogStyle = {
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
-
   return (
-    <div>
+    <div className='grid gap-1 m-12'>
       {blogs.map((blog) => (
-        <div style={blogStyle} key={blog.id}>
-          <Link to={`/blogs/${blog.id}`}>
-            {blog.title}
+        <div key={blog.id} className='hover:bg-slate-100 w-full h-16 border rounded-lg'>
+          <Link to={`/blogs/${blog.id}`} >
+            <Typography type='h5' className='ml-5 py-3'>
+              {blog.title}
+            </Typography>
           </Link>
         </div>
       ))}
