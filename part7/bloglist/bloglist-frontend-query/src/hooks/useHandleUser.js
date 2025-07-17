7c57fd8d97793917872fd45913b02140330b6794
himumi <erliansyaha@gmail.com>
2025-07-17 09:05:52 +0700

    refactor: rename useUser to useHandleUser & separate handleLogin and handleLogout to their hooks
    useHandleLogin
    useHandleLogout

--- part7/bloglist/bloglist-frontend-query/src/hooks/useHandleUser.js
+++ part7/bloglist/bloglist-frontend-query/src/hooks/useHandleUser.js
@@ -6,25 +6,21 @@ import loginServices from '../services/login'
 
 import useNotification from './useNotification'
 
-const useUser = () => {
+const setUser = (user) => {
+  return {
+    type: 'SET_USER',
+    payload: user
+  }
+}
+
+const removeUser = () => {
+  return {
+    type: 'REMOVE_USER'
+  }
+}
+
+const useHandleUser = () => {
   const { state, dispatch } = useContext(ReducerContext)
-  const { 
-    setSuccessNotification,
-    setErrorNotification
-  } = useNotification()
-
-  const setUser = (user) => {
-    return {
-      type: 'SET_USER',
-      payload: user
-    }
-  }
-
-  const removeUser = () => {
-    return {
-      type: 'REMOVE_USER'
-    }
-  }
 
   useEffect(() => {
     const loggedUser = window.localStorage.getItem('loggedBlogAppUser')
@@ -35,7 +31,19 @@ const useUser = () => {
     }
   }, [])
 
-  const handleLogin = async credential => {
+  return {
+    value: state.user
+  }
+}
+
+export const useHandleLogin = () => {
+  const { state, dispatch } = useContext(ReducerContext)
+  const { 
+    setSuccessNotification,
+    setErrorNotification
+  } = useNotification()
+
+  return async credential => {
     try {
       const user = await loginServices.login(credential)
       window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
@@ -46,8 +54,13 @@ const useUser = () => {
       setErrorNotification(`Failed login`, 3)
     }
   } 
+}
 
-  const handleLogout = () => {
+export const useHandleLogout = () => {
+  const { state, dispatch } = useContext(ReducerContext)
+  const { setSuccessNotification } = useNotification()
+  
+  return () => {
     // delete all user information from app
     dispatch(removeUser())
     window.localStorage.removeItem('loggedBlogAppUser')
@@ -55,12 +68,6 @@ const useUser = () => {
 
     setSuccessNotification('Logged out', 3)
   }
-
-  return {
-    value: state.user,
-    handleLogin,
-    handleLogout
-  }
 }
 
-export default useUser
+export default useHandleUser
