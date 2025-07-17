d0bba0b7fe5b282143f2699d45ae4c6f7fa21b23
himumi <erliansyaha@gmail.com>
2025-07-17 21:51:22 +0700

    ui: add styles to Comments

--- part7/bloglist/bloglist-frontend-query/src/components/comment/Comments.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/comment/Comments.jsx
@@ -1,10 +1,15 @@
+import { Typography } from "@material-tailwind/react"
 import CommentForm from "./CommentForm"
 import CommentList from "./CommentList"
 
 const Comments = ({ blog }) => {
   return (
     <div>
-      <h3>comments</h3>
+      <Typography
+        className='text-base my-4 font-medium' 
+      >
+        Comments
+      </Typography>
       <CommentForm />
       <CommentList comments={blog.comments} />
     </div>
