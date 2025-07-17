6d541f1ec5f51b4a294468f992b0717f76012c24
himumi <erliansyaha@gmail.com>
2025-07-17 21:04:42 +0700

    ui: add styles to User

--- part7/bloglist/bloglist-frontend-query/src/components/users/User.jsx
+++ part7/bloglist/bloglist-frontend-query/src/components/users/User.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useQueryClient } from "@tanstack/react-query"
+import { Card, List, ListItem, Typography } from "@material-tailwind/react"
+import { Link } from "react-router-dom"
 
 const User = () => {
   const queryClient = useQueryClient()
@@ -13,14 +15,34 @@ const User = () => {
   }
 
   return (
-    <div>
-      <h2>{user.name}</h2>
-      <span><strong>added blogs</strong></span>
-      <ul>
-        {user.blogs.map(blog => 
-          <li key={blog.id}>{blog.title}</li>
-        )}
-      </ul>
+    <div className="flex justify-center my-32">
+      <Card variant='solid' className="w-1/3 px-6 py-12">
+        <Typography type="h2" className='mx-10 mb-10'>
+          {user.name}
+        </Typography>
+        <div className="w-full my-6 mx-6 ">
+          <Typography className='font-medium text-lg pl-8 mb-4'>
+            Added Blogs
+          </Typography>
+          <List className='mt-7'>
+            {user.blogs.map(blog => 
+              <ListItem 
+                key={blog.id}
+                className="px-8 py-4 my-1 mx-8"
+              >
+                <Link to={`/blogs/${blog.id}`}>
+                  {blog.title}
+                </Link>
+              </ListItem>
+            )}
+            {user.blogs.length === 0 && 
+              <Typography className='px-8 my-4 mx-8'>
+                no content
+              </Typography>
+            }
+          </List>
+        </div>
+      </Card>
     </div>
   )
 }
